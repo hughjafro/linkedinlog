f3c125a5c4b2a0195fa2679ed7b7e40566888e4c
Christopher Tolbert <cmtolbert1@gmail.com>
2013-11-22 11:28:29 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linkedinlog::Application.config.secret_key_base = '2e0bebeebbf5fc8113e14bc0b620f9d8f2c7bf1fd70bd1f3edbc4a79d832122b1e4fd8aca876301b68185e3e567dea70a2aa4e03e53e06b1cebf62e40c170720'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Linkedinlog::Application.config.session_store :cookie_store, key: '_linkedinlog_session'
